b46b345a14e2661e9db5b75a9b80c5afe5486a2c
mayur <mayurwarghane2512@gmail.com>
2023-07-12 18:05:58 +0530

    add another String

--- src/StringDemo.java
+++ src/StringDemo.java
@@ -3,6 +3,7 @@ public class StringDemo {
 
 	public static void main(String[] args) {
 		System.out.println("First git project in java");
+		System.out.println("Mayur and sagar metting in google meet to learn some code for mayu");
 
 	}
 
